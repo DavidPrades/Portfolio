b9f77bde534062faa40cfe502b460bb67972f992
DavidPrades <davidprades8@gmail.com>
2023-02-19 16:47:37 +0100

    Acabada navbar stilos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LandingPageComponentComponent } from './components/landing-page-component/landing-page-component.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponentComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
